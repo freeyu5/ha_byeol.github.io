86c1db30a67e5f561b9ec17a5ad6e0429fb0ab01
freeyu5 <q528398935@Gmail.com>
2019-12-26 01:51:48 +0800

    新的上传

---
+++ jinrituijian.js
@@ -0,0 +1,5 @@
+document.getElementById("tuijianbiaoti").innerHTML="05年韩国小萌妹";
+document.getElementById("tuijiantupian1").src="img/1.jpg";
+document.getElementById("tuijiantupian1").style.width="100%";
+document.getElementById("tuijiantupian2").src="img/2.jpg";
+document.getElementById("tuijiantupian2").style.width="100%";
