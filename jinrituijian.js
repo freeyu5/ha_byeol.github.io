77024925135643b908794a672d116880e378a303
freeyu5 <q528398935@Gmail.com>
2019-12-30 04:03:11 +0800

    图片可点击查看
    我真他么太有耐心了  啦啦啦~~

--- jinrituijian.js
+++ jinrituijian.js
@@ -1,36 +1,87 @@
 ﻿document.getElementById("tuijianbiaoti1").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian1").src="img/1.jpg";
 document.getElementById("tuijiantupian1").style.width="100%";
+tuijiantupian1.onclick = function(){
+	var tuSrc = tuijiantupian1.getAttribute("src");
+	window.open(src = tuSrc)
+};
+
 document.getElementById("tuijianbiaoti2").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian2").src="img/2.jpg";
 document.getElementById("tuijiantupian2").style.width="100%";
+tuijiantupian2.onclick = function(){
+	var tuSrc2 = tuijiantupian2.getAttribute("src");
+	window.open(src = tuSrc2)
+};
+
+
 document.getElementById("tuijianbiaoti3").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian3").src="img/3.jpg";
 document.getElementById("tuijiantupian3").style.width="100%";
+tuijiantupian3.onclick = function(){
+	var tuSrc3 = tuijiantupian3.getAttribute("src");
+	window.open(src = tuSrc3)
+};
+
+
 document.getElementById("tuijianbiaoti4").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian4").src="img/4.jpg";
 document.getElementById("tuijiantupian4").style.width="100%";
+tuijiantupian4.onclick = function(){
+	var tuSrc4 = tuijiantupian4.getAttribute("src");
+	window.open(src = tuSrc4)
+};
+
+
 document.getElementById("tuijianbiaoti5").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian5").src="img/5.jpg";
 document.getElementById("tuijiantupian5").style.width="100%";
+tuijiantupian5.onclick = function(){
+	var tuSrc5 = tuijiantupian5.getAttribute("src");
+	window.open(src = tuSrc5)
+};
+
+
 document.getElementById("tuijianbiaoti6").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian6").src="img/6.jpg";
 document.getElementById("tuijiantupian6").style.width="100%";
+tuijiantupian6.onclick = function(){
+	var tuSrc6 = tuijiantupian6.getAttribute("src");
+	window.open(src = tuSrc6)
+};
+
+
 document.getElementById("tuijianbiaoti7").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian7").src="img/7.jpg";
 document.getElementById("tuijiantupian7").style.width="100%";
+tuijiantupian7.onclick = function(){
+	var tuSrc7 = tuijiantupian7.getAttribute("src");
+	window.open(src = tuSrc7)
+};
+
+
 document.getElementById("tuijianbiaoti8").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian8").src="img/8.jpg";
 document.getElementById("tuijiantupian8").style.width="100%";
+tuijiantupian8.onclick = function(){
+	var tuSrc8 = tuijiantupian8.getAttribute("src");
+	window.open(src = tuSrc8)
+};
+
+
 document.getElementById("tuijianbiaoti9").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian9").src="img/9.jpg";
 document.getElementById("tuijiantupian9").style.width="100%";
+tuijiantupian9.onclick = function(){
+	var tuSrc9 = tuijiantupian9.getAttribute("src");
+	window.open(src = tuSrc9)
+};
+
+
 document.getElementById("tuijianbiaoti10").innerHTML="05Girls Img";
 document.getElementById("tuijiantupian10").src="img/10.jpg";
 document.getElementById("tuijiantupian10").style.width="100%";
-document.getElementById("tuijianbiaoti5").innerHTML="05Girls Img";
-document.getElementById("tuijiantupian5").src="img/5.jpg";
-document.getElementById("tuijiantupian5").style.width="100%";
-document.getElementById("tuijianbiaoti6").innerHTML="05Girls Img";
-document.getElementById("tuijiantupian6").src="img/6.jpg";
-document.getElementById("tuijiantupian6").style.width="100%";
+tuijiantupian10.onclick = function(){
+	var tuSrc10 = tuijiantupian10.getAttribute("src");
+	window.open(src = tuSrc10)
+};
